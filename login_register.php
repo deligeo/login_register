88bdb6b47aab2dcfe3e6203877916696a4f7face
deligeo <georgedelna10@gmail.com>
2026-02-16 22:54:49 -0500

    Added login functionality to login_register.php

--- login_register.php
+++ login_register.php
@@ -25,4 +25,25 @@ if(isset($_POST['register'])) {
   header('Location: index.php');
   exit();
 }
+
+if(isset($_POST['login'])) {
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+  $result = $conn->query("SELECT * FROM users WHERE email = '$email'");
+  
+  if($result->num_rows > 0) {
+    $user = $result->fetch_assoc();
+    if(password_verify($password, $user['password'])) {
+      $_SESSION['name'] = $user['name'];
+      $_SESSION['email'] = $user['email'];
+      header('Location: user_page.php');
+      exit();
+    } 
+  }
+  $_SESSION['login_error'] = 'Invalid email or password';
+  $_SESSION['active_form'] = 'login';
+  header('Location: index.php');
+  exit();
+}
 ?>
