abb1602fea477a8ccc5caccb82418c8ef9f48fd4
deligeo <georgedelna10@gmail.com>
2026-02-16 23:05:13 -0500

    Added php elements and changes to index file

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?php
+
+session_start();
+
+$errors = [
+  'login' => $_SESSION['login_error'] ?? '',
+  'register' => $_SESSION['register_error'] ?? ''
+];
+
+$activeForm = $_SESSION['active_form'] ?? 'login';
+
+session_unset();
+
+function showError($error) {
+  return !empty($error) ? "<p class='error-message'>$error</p>" : '';
+}
+
+function isActiveForm($formName, $activeForm) {
+  return $formName === $activeForm ? 'active' : '';
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,9 +31,10 @@
 </head>
 <body>
   <div class="container">
-    <div class="form-box active" id="login-form">
+    <div class="form-box <?= isActiveForm('login', $activeForm); ?>" id="login-form">
       <form action="login_register.php" method="post">
         <h2>Login</h2>
+        <?= showError($errors['login']); ?>
         <input type="email" name="email" placeholder="Email" required>
         <input type="password" name="password" placeholder="Password" required>
         <button type="submit" name="login">Login</button>
@@ -18,9 +42,10 @@
       </form>
     </div>
 
-    <div class="form-box" id="register-form">
+    <div class="form-box <?= isActiveForm('register', $activeForm); ?>" id="register-form">
       <form action="login_register.php" method="post">
         <h2>Register</h2>
+        <?= showError($errors['register']); ?>
         <select name="school" required>
           <option value="">--Select School--</option>
           <option value="school1">Rajagiri Public School, Kochi</option>
